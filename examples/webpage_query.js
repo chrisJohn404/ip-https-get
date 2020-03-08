34e400c8e1485e6ecf299cb4c2653df55f1a3e44
Chris <chris.s.johnson4@gmail.com>
2020-03-08 16:02:56 -0600

    Added more examples and extra error checking to HTTP requests.

--- examples/webpage_query.js
+++ examples/webpage_query.js
@@ -1,5 +1,8 @@
 
-
+/*
+ * Note: This library doesn't support standard web-page queries at this time as the 
+ * content-length header is not supplied in the http header.
+ */
 const get = require('../lib/ip-https-get.js').get;
 
 
@@ -8,11 +11,9 @@ const options = {
 	// ip: '104.26.1.235',
 	ip: '::ffff:104.26.0.235',
 	hostname: 'labjack.com',
-	path: '/sites/default/files/styles/products_homepage/public/U3HV_white_shadow.JPG',
-	reqContentType: 'image/jpeg',
+	path: '/support/firmware/t7',
 	pubkey256: 'aTtriJnaZ8G2PTSu6uPx1drsS975UnP88Sos4mNSVWw=',
 	cert256: '39:24:D6:18:3C:41:38:F9:4D:D0:4E:4D:8A:BF:D7:39:9C:73:F6:A1:A7:53:7E:C5:FF:5F:52:22:A8:3D:CD:B7',
-	maxHdrSize: 800,
 };
 
 get(options, function(err, res) {
